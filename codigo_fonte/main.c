666c76b1b239968a8545dbbbf3dcedd9976a7440
brenda_braganca <bragancadeassis@gmail.com>
2025-06-30 10:02:54 -0300

    teste de funcionamento da biblioteca e estruturação basica do trabalho

---
+++ codigo_fonte/main.c
@@ -0,0 +1,13 @@
+#include "raylib.h"
+
+int main(void) {
+    InitWindow(800, 450, "Hello Raylib");
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, Raylib!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
