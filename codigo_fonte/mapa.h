0ab67df9be66bcf6a5c5f3d3393c1b8c2be1be2a
brenda_braganca <bragancadeassis@gmail.com>
2025-06-30 13:45:58 -0300

    corrigindo erro de cabecalho

--- codigo_fonte/mapa.h
+++ codigo_fonte/mapa.h
@@ -1,6 +1,9 @@
-#ifndef MAPA_H //  independentemente de quantas vezes esse arquivo de cabecalho seja incluso, ele sera rodado apenas uma vez
 #define MAPA_H
 
 //tamanhos especificados no roteiro do trabalho
 #define LINHAS 25
 #define COLUNAS 60
+
+char **inicializarMapa(const char *path);
+void finalizarMapa(char **mapa);
+
