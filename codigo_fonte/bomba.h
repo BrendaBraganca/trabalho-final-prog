3c588c35d9e30e425a3923f94a24265540888676
brenda_braganca <bragancadeassis@gmail.com>
2025-07-03 13:21:30 -0300

    conserto de bug

--- codigo_fonte/bomba.h
+++ codigo_fonte/bomba.h
@@ -2,4 +2,22 @@
 #include "posicao.h"
 #define BOMBA_H
 #define MAX_BOMBAS 3
-#define TEMPO_EXPLO
+#define TEMPO_EXPLOSAO 3.0f
+#define RAIO_EXPLOSAO 100
+
+typedef struct {
+    tipoPosicao posicao;
+    float tempoRestante; //quanto falta para explodir
+    int ativa;
+    int explodindo; 
+    float tempoExplosao; // cronometro para explosao
+} tipoBomba;
+
+void inicializarBombas(tipoBomba bombas[]);
+int plantarBombas(tipoBomba bombas[], tipoPosicao posicao, int bombasRestantes); // retorna se a bomba conseguiu ser plantada ou não
+void atualizarBombas(tipoBomba bombas[], float intervalo); 
+void desenharBombas(tipoBomba bombas[]);
+
+
+
+
